53b8cd82b3602d7b6333a73bb6cf657bfeda4853
github username <github email address>
2019-12-09 21:56:29 -0500

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ document.addEventListener("keydown", function(e) {
 });
 
 function moveDodgerRight() {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+    let rightNumbers = dodger.style.right.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
 
-    if (left > 0) {
-        dodger.style.left = `${left + 1}px`;
+    if (right < 0) {
+        dodger.style.right = `${right + 1}px`;
     }
 }
 
